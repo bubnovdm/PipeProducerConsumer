f859609b8962a7756b63984b4082a97cc2590881
dbubnov <dbubnov@nbiservice.ru>
2025-11-07 16:10:15 +0300

    upd. + горутины.

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"context"
+	"sync"
+)
+
 /*
 Описание
 Нам нужно передать данные из некоторого источника некоторому потребителю. При этом источник отдает данные небольшими пачками (~ десятки записей), а потребитель оптимальнее работает с крупными батчами (~ тысячи записей). Реальный пример - поставка данных из очередей типа Kafka в базу Clickhouse.
@@ -53,48 +58,136 @@ func Pipe(p Producer, c Consumer) error {
 	// 4 - Коммитим имеющиеся данные (в цикле - может быть накоплено несколько cookie)
 	// 5 - Продолжаем принимать данные
 
+	// -----------------------------------------------------------------------------------------------------------------
+
 	// Слайс для батчей
 	buffer := make([]any, 0, MaxItems)
 	// Слайс для куки
 	var cookies []int
+	// Добавил структуру, которую будем передавать в канал (сразу и слайс данных и куки, которые надо закоммитить)
+	type batch struct {
+		items  []any
+		cookie []int
+	}
+	// Канал, через который будем передавать батчи из продюссера в консюмер
+	// Хотя предположу, что тут будет лучше буферизированный канал, т.к. если мы будем получать данные от источника быстрее
+	// чем вторая горутина будет их коммитить, то первая горутина будет тут заблокирована до последующего чтения.
+	// Но тогда, наверное, и буферезированный не панацея, т.к. рано или поздно буфер всё равно заьтётся.
+	butchCh := make(chan batch)
+	// Ошибка для возврата из функции
+	var firstError error
+	// Мютекс для записи в ошибку
+	var errMu sync.Mutex
+	// wg для наших горутин
+	var wg sync.WaitGroup
+	// Контекст для отмены по ошибке
+	ctx, cancel := context.WithCancel(context.Background())
+	/*
+		Нашёл интересную вещь по завершению, можно сделать контекст через:
+		ctx, cancel := signal.NotifyContext(
+			context.Background(),
+			syscall.SIGTERM,
+			syscall.SIGHUP,
+			syscall.SIGINT,
+			syscall.SIGQUIT,
+		)
+		Возможно такой вариант будет даже чуть интереснее простого контекста с отменой.
 
-	for {
-		items, cookie, err := p.Next()
+	*/
 
-		if err != nil {
-			return err
-		}
+	// -----------------------------------------------------------------------------------------------------------------
 
-		// Если источник пустой, просто продолжаем
-		if len(items) == 0 {
-			continue
-			/*
-				Т.к. источник условно бесконечный, случай, что данные из источника больше не поступают
-				и надо отправить остатки не обрабатываем.
-			*/
-		}
+	/*
+		Так, надо поделить код на две горутины:
+		1) Читаем источник через Next(), соединяем данные и проверяем длину. Когда надо - пишем наш батч в канал.
+		2) Ждёт данные из канала, когда получает - запускаем Process() и Commit().
+	*/
 
-		// Если не взелаем - отправляем, обнуляем слайсы и добавляем данные в них
-		if (MaxItems - len(buffer)) < len(items) {
-			// Случай, когда мы отправляем уже накопленные данные
-			err := c.Process(buffer)
-			if err != nil {
-				return err
+	// 1-ая горутина
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(butchCh)
+
+		for {
+			select {
+			case <-ctx.Done():
+				// Перед выходом отправим, что накопилось
+				if len(buffer) > 0 {
+					butchCh <- batch{items: buffer, cookie: cookies}
+					// Нужно ли тут самим чистить остатки буфера и куки? Или GC подчистит за нами?
+				}
+				return
+			default:
 			}
-			for _, v := range cookies {
-				err := p.Commit(v)
-				if err != nil {
-					return err
+
+			items, cookie, err := p.Next()
+
+			// Тут теперь не просто проверяем на ошибку, а пишем её в переменную firstError, которую вернём из функции
+			// и отменяем контекст
+			if err != nil {
+				errMu.Lock()
+				if firstError == nil {
+					firstError = err
+					cancel()
+				}
+				errMu.Unlock()
+				return
+			}
+
+			// Если источник пустой, просто продолжаем
+			if len(items) == 0 {
+				continue
+			}
+
+			// Если не влезаем, то пишем наши слайсы в структуру батча и кладём её в канал
+			if (MaxItems - len(buffer)) < len(items) {
+				butchCh <- batch{items: buffer, cookie: cookies}
+				buffer = buffer[:0]
+				cookies = cookies[:0]
+			}
+
+			buffer = append(buffer, items...)
+			cookies = append(cookies, cookie)
+
+		}
+	}()
+
+	// 2-ая горутина
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		for b := range butchCh {
+			if err := c.Process(b.items); err != nil {
+				errMu.Lock()
+				if firstError == nil {
+					firstError = err
+					cancel()
+				}
+				errMu.Unlock()
+				return
+			}
+			for _, c := range b.cookie {
+				if err := p.Commit(c); err != nil {
+					errMu.Lock()
+					if firstError == nil {
+						firstError = err
+						cancel()
+					}
+					errMu.Unlock()
+					return
 				}
 			}
-			// Обнуляем оба слайса
-			buffer = buffer[:0]
-			cookies = cookies[:0]
-
 		}
+	}()
 
-		// Просто добавляем в буфер, т.к. есть ещё место
-		buffer = append(buffer, items...)
-		cookies = append(cookies, cookie)
-	}
+	wg.Wait()
+	return firstError
 }
